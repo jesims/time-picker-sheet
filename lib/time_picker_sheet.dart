1845d7cb33f90404f850f58324cb2c8674d3386e
Alexander R Scott <xander@axrs.io>
2023-02-08 15:53:40 +1000

    Style: Add Library Export

---
+++ lib/time_picker_sheet.dart
@@ -0,0 +1,6 @@
+/// A Time picker sheet library for flutter. You can also customize the widget
+/// inside the sheet to align with your requirement.
+library time_picker_sheet;
+
+export 'widget/sheet.dart';
+export 'widget/time_picker.dart';
